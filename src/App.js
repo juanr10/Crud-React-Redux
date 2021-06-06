5f1e69726d136c5b8199b698061de89f979a769c
juanr10 <argudojuan7@gmail.com>
2021-06-06 14:01:53 +0200

    Installing & defining Redux structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Fragment } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+//Redux
+import {Provider} from 'react-redux';
+import store from './store';
 //Components
 import Header from './components/Header';
 import Products from './components/Products';
 import NewProduct from './components/NewProduct';
 import EditProduct from './components/EditProduct';
 
+
 /**
- * CRUD React Usando react-router-dom, Redux, json server
- * @returns 
+ * @name: .
+ * @description: CRUD React Usando react-router-dom, Redux, json server.
+ * @author: Juan Argudo.
+ * @version: .
  */
 function App() {
   return (
     <Router>
-      <Header />
-        <div className="container mt-5">
-          <Switch>
-              <Route exact path="/" component={Products}/>
-              <Route exact path="/productos/nuevo" component={NewProduct}/>
-              <Route exact path="/productos/editar/:id" component={EditProduct}/>
-          </Switch>
-        </div>
+      <Provider store={store}>
+        <Header />
+          <div className="container mt-5">
+            <Switch>
+                <Route exact path="/" component={Products}/>
+                <Route exact path="/productos/nuevo" component={NewProduct}/>
+                <Route exact path="/productos/editar/:id" component={EditProduct}/>
+            </Switch>
+          </div>
+      </Provider>
     </Router>
   );
 }
---
+++ src/reducers/productsReducer.jsx
@@ -0,0 +1,13 @@
+//Each reducer has his own state
+const initialState = {
+    products: [],
+    error: false,
+    loading: false
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
